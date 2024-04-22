25f61ced66f1a126cee56086f20335bf9d5ba965
wildan daffa <wildandaffa078@gmail.com>
2024-04-22 11:07:12 +0700

    first commit

---
+++ lib/sample.dart
@@ -0,0 +1,74 @@
+
+
+class sample {
+  final String? name;
+  final int? age;
+  final List<String>? hobby;
+  final Github? github;
+  final List<Article>? articles;
+
+  sample({this.name, this.age, this.hobby, this.github, this.articles,});
+
+  factory sample.fromJson(Map<String, dynamic> json) {
+    return sample(
+      name: json["name"] ?? '',
+      age: json["age"] ?? '',
+      hobby: List<String>.from(json["hobby"] ?? []),
+      github: Github.fromJson(json["github"]),
+      articles: List<Article>.from(
+          json["articles"].map((article) => Article.fromJson(article)) ?? []),
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Sample(name: $name, age: $age, hobby: $hobby, github: $github)';
+  }
+}
+
+class Github {
+  final String? username;
+  final int? repositories;
+  final bool? isGDE;
+
+  Github({
+    required this.username,
+    required this.repositories,
+    required this.isGDE,
+  });
+
+  factory Github.fromJson(Map<String, dynamic> json) {
+    return Github(
+      username: json["username"],
+      repositories: json["repositories"],
+      isGDE: json["isGde"],
+    );
+  }
+
+  @override
+  String toString() =>
+      'Github(username: $username, repositories: $repositories, isGDE: $isGDE)';
+}
+
+class Article {
+  final int? id;
+  final String? title;
+  final String? subtitle;
+
+  Article({
+    this.id,
+    this.title,
+    this.subtitle,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      id: json["id"] ?? 0,
+      title: json["title"] ?? '',
+      subtitle: json["subtitle"] ?? '',
+    );
+  }
+
+  @override
+  String toString() => 'Article(id: $id, title: $title, subtitle: $subtitle)';
+}
